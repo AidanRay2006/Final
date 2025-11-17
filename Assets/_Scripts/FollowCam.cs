3bc3dcd7605147adf5d3f1f20d1c40c95d77fe5c
AidanRay2006 <aray5@ltu.edu>
2025-11-17 15:41:38 -0500

    Added the dash mechanic
    It feels good enough, but I may change it later on, just like the movement.

--- Assets/_Scripts/FollowCam.cs
+++ Assets/_Scripts/FollowCam.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour
 {
     public GameObject follow;
+    public float zDistance = 10;
+    public float allowableOffset;
+    public float speed = 12f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(follow.transform.position.x, follow.transform.position.y, -10);
+        //stolen from assignment 4
+        if (Vector3.Distance(transform.position, follow.transform.position + Vector3.back * zDistance) > allowableOffset)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, follow.transform.position + Vector3.back * zDistance, speed * Time.deltaTime);
+        }
     }
 }
--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -10,11 +10,15 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 12f;
     public float speed = 6f;
     public float deltaSpeed = 6f;
+    public float dashForce = 12f;
 
     //private variables
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private BoxCollider2D groundCollider;
+    private bool dashed;
+    private bool touchedGround;
+    private float dashRecharge;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +29,9 @@ public class PlayerMovement : MonoBehaviour
         groundCollider = GetComponent<BoxCollider2D>();
 
         sr = GetComponent<SpriteRenderer>();
+
+        dashed = false;
+        dashRecharge = 0;
     }
 
     // Update is called once per frame
@@ -87,9 +94,41 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //handles the dash
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X) && !dashed)
         {
+            if (sr.flipX)
+            {
+                rb.AddForce(Vector2.left * dashForce, ForceMode2D.Impulse);
+            }
+            else
+            {
+                rb.AddForce(Vector2.right * dashForce, ForceMode2D.Impulse);
+            }
+            dashed = true;
+            touchedGround = false;
+            dashRecharge = 0;
+        }
 
+        //make sure enough time has passed between when the player last dashed
+        //so that they can dash again, and they have touched the ground since then
+        if (dashed && dashRecharge == 0.5f && touchedGround)
+        {
+            dashed = false;
+        }
+        else
+        {
+            dashRecharge += Time.deltaTime;
+            //dashRecharge cannot exceed 0.5
+            if (dashRecharge >= 0.5f)
+            {
+                dashRecharge = 0.5f;
+            }
+        }
+        //fixes a bug where if you had touched the ground, but tried to dash
+        //in the air before the time ran out you couldn't
+        if (Grounded())
+        {
+            touchedGround = true;
         }
     }
 
